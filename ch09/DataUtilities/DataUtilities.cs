fa78acfc8bc821869521cea4db8626e58c620310
David Stevenson <drstevenson1958@yahoo.com>
2023-08-28 07:56:16 -0400

    Chapter 9: Scripting in SSIS

---
+++ ch09/DataUtilities/DataUtilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace SSISUtilityLib_CSharp
+{
+    public class DataUtilities
+    {
+        public static bool IsValidUSPostalCode(string PostalCode)
+		{
+            return Regex.IsMatch(PostalCode, "^[0-9]{5}(-[0-9]{4})?$");
+		}
+    }
+}
